bfaa4f97fbf6fcf35a3193c7dc9931fe0f678915
gary <gary.white@lodgit.com>
2022-04-11 12:20:54 +0200

    code

---
+++ app.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return
+}
+
+function divide(a, b) {
+    return
+}
+
+function multiply(a, b) {
+    return
+}
+
+function mod(a, b) {
+    return
+}
+
+function exponent(a, b) {
+    return
+}
+
+function subt(a, b) {
+    return
+}
+
+console.log(add(1, 2)); // 3
+console.log(multiply(1, 2)); // 2
+console.log(divide(2, 1)); // 2
+console.log(mod(1, 2)); // 1
+console.log(exponent(1, 2)); // 1
+console.log(subt(1, 2)); // -1
